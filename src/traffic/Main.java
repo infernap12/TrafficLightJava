0cb076c922010a72df87481e6cb6533f39c986da
james <james1.6@hotmail.com>
2024-01-05 07:07:35 +1030

    Stage 1

--- src/traffic/Main.java
+++ src/traffic/Main.java
@@ -1,7 +1,32 @@
 package traffic;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Main {
-  public static void main(String[] args){
-    System.out.println("Hello World!");
-  }
+
+    static Map<Integer, String> Menu = new LinkedHashMap<>();
+
+
+    public static void main(String[] args) {
+        initMenu();
+        System.out.println("Welcome to the traffic management system!");
+        printMenu();
+    }
+
+    private static void initMenu() {
+        Menu.put(1, "Add");
+        Menu.put(2, "Delete");
+        Menu.put(3, "System");
+        Menu.put(0, "Quit");
+    }
+
+    private static void printMenu() {
+        System.out.println("Menu:");
+        for (Map.Entry<Integer, String> entry : Menu.entrySet()) {
+            System.out.printf("%d. %s%n", entry.getKey(), entry.getValue());
+        }
+    }
+
+
 }
