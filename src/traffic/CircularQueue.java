1dc342b1f1a9deb2f805cba3e35483737619851c
james <james1.6@hotmail.com>
2024-01-10 00:51:16 +1030

    Stage 5 Basic Adding and removing of raods;

---
+++ src/traffic/CircularQueue.java
@@ -0,0 +1,20 @@
+package traffic;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.LinkedList;
+
+
+public class CircularQueue extends LinkedList<String>{
+
+
+    public CircularQueue() {
+    }
+
+    public CircularQueue(Collection<? extends String> c) {
+        super(c);
+    }
+    public void next() {
+
+    }
+}
--- src/traffic/Main.java
+++ src/traffic/Main.java
@@ -2,6 +2,7 @@ package traffic;
 
 import java.io.Console;
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -10,39 +11,75 @@ public class Main {
     static int roads;
     static int interval;
 
+    static State state = State.NOT_STARTED;
 
-    public static void main(String[] args) {
+    static LinkedList<String> queue = new LinkedList<>();
+
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Welcome to the traffic management system!");
         System.out.print("Input the number of roads:");
         roads = input.askInitials();
         System.out.print("Input the interval:");
         interval = input.askInitials();
-        while (true) {
+
+        state = State.MENU;
+
+        Thread worker = new Thread(new TrafficSystem(roads,interval));
+        worker.setName("QueueThread");
+        worker.start();
+
+
+
+        while (state == State.MENU) {
             menu.printOptions();
             String choice = input.askLine().trim();
             switch (choice) {
-                case "1" -> System.out.println("Road added");
-                case "2" -> System.out.println("Road deleted");
-                case "3" -> System.out.println("System opened");
+                case "1" -> {
+                    System.out.print("Input road name: ");
+                    String element = input.askLine();
+                    if (queue.size() >= roads) {
+                        System.out.println("Error: queue is full");
+                    } else {
+                        queue.add(element);
+                        System.out.println("%s Added.".formatted(element));
+                    }
+                }
+                case "2" -> {
+                    if (queue.isEmpty()) {
+                        System.out.println("Error: queue is empty");
+                    } else {
+                        System.out.println("%s deleted".formatted(queue.removeFirst()));
+                    }
+                }
+                case "3" -> {
+                    //System.out.println("System opened");
+                    state = State.SYSTEM;
+                }
                 case "0" -> {
                     System.out.println("Bye!");
+                    worker.interrupt();
+                    worker.join();
                     System.exit(0);
                 }
                 default -> System.out.println("Incorrect option");
             }
             waitForEnter("");
-            try {
-                var clearCommand = System.getProperty("os.name").contains("Windows")
-                        ? new ProcessBuilder("cmd", "/c", "cls")
-                        : new ProcessBuilder("clear");
-                clearCommand.inheritIO().start().waitFor();
-            }
-            catch (IOException | InterruptedException ignored) {
-
-            }
+            state = State.MENU;
+            clear();
 
         }
+    }
 
+    static void clear() {
+        try {
+            var clearCommand = System.getProperty("os.name").contains("Windows")
+                    ? new ProcessBuilder("cmd", "/c", "cls")
+                    : new ProcessBuilder("clear");
+            clearCommand.inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException ignored) {
+
+        }
     }
 
     // ...
@@ -56,14 +93,20 @@ public class Main {
             c.readLine();
         } else {
 
-            try{
+            try {
                 //noinspection ResultOfMethodCallIgnored
-                System.in.read();}
-            catch(Exception e){
+                System.in.read();
+            } catch (Exception e) {
                 System.out.printf("nope, we threw %s%n", e);
             }
 
         }
+    }
+
+    enum State {
+        NOT_STARTED,
+        MENU,
+        SYSTEM;
     }
 }
 
